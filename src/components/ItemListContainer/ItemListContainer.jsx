40a5373808cb84a9647f73138811bf5bf07c7d43
Juan Antonio Flores <juanflores0796@gmail.com>
2023-10-09 20:43:10 -0300

    se agrego hook y prop

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,16 +1,28 @@
-import { useState } from "react"
-import Inicio from "./Inicio/Inicio"
-import Contacto from "./Contacto/Contacto"
-import Nosotros from "./Nosotros/Nosotros"
-
-const ItemListContainer = () => {
+import { useState, useEffect } from "react"
 
 
+const ItemListContainer = ({nombre}) => {
+  const [mostrarSaludo, setMostrarSaludo] = useState(false);
+  const [contador, setContador] = useState(0)
+  const ss = () => {
+    setMostrarSaludo(!mostrarSaludo)
+  }
 
+  useEffect(() => {
+    setContador(contador + 1)
+  },[mostrarSaludo])
+  useEffect(() => {
+    setContador(contador) //por unica vez reinicializo el contador cuando se monta para que sea = 0
+  },[])
     return(
-      <></>
-        )
-        
-    }
+        <>
+        <button onClick={ss} className="botonSaludo">¡PULSAME!</button>
+        {mostrarSaludo && <p className="saludo">{nombre}</p>}
+        {(contador>=10) && <p className="alarmaSaludos">Deja el botón en paz... Por favor</p>}
+        <p>Veces que se apretó el botón: {contador}</p>
+        </>
+    )
+}
+
     
     export default ItemListContainer
